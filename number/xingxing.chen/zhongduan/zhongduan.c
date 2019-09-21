7501d452a047960fd8cea979c82e24b0ea9acf06
chenxingxing2 <3267308090@qq.com>
2019-09-21 16:43:47 +0800

    add zhongduan

--- number/xingxing.chen/zhongduan/zhongduan.c
+++ number/xingxing.chen/zhongduan/zhongduan.c
@@ -1,24 +1,25 @@
 #include <reg51.h>
 #include <intrins.h>
-sbit p1_0=P1^0;
-void delay (void)
+sbit p1=P1^0;
+void delay(void)
 {
-	int a=5000;
-	while(a--)_ncp_();
+	int a=500;
+	int i;
+	for(i=0;i<a;i++);
 }
-void INT0_srv (void)©iinterrupt 0 using 1
+void INT0_srv(void) interrupt 0 using 1
 {
-	delay();
+delay();
 	if(INT0==0)
 	{
-		p1_0=!p1_0;
-		while(INT0= =0);
+		p1=! p1;
+   while (INT0==0);
 	}
+ }
+void main()
+{
+p1=0;
+	EA=1;
+	EX0=1;                                    
+	while(1);
 }
-	void main()
-	{
-		p1_0=0;
-		EA=1;
-		EX0=1;
-		while(1);
-	}
